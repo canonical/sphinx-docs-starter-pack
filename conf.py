33e8a0cde61458ec0732b4525042738a6621f936
Ruth Fuchss <ruth.fuchss@canonical.com>
2023-05-26 15:11:10 +0200

    document all variables users might need to change
    Signed-off-by: Ruth Fuchss <ruth.fuchss@canonical.com>

--- conf.py
+++ conf.py
@@ -21,10 +21,13 @@ ogp_image = "https://assets.ubuntu.com/v1/253da317-image-document-ubuntudocs.svg
 
 html_context = {
     # Change to the discourse instance you want to be able to link to
+    # (use an empty value if you don't want to link)
     "discourse": "https://discourse.ubuntu.com",
     # Change to the GitHub info for your project
     "github_url": "https://github.com/canonical/starter-pack",
+    # Change to the branch for this version of the documentation
     "github_version": "main",
+    # Change to the folder that contains the documentation (usually "/" or "/docs/")
     "github_folder": "/",
     # Change to an empty value if your GitHub repo doesn't have issues enabled
     "github_issues": "enabled"
