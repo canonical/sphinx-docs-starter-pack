4d6020c9b12baff01567d24cc8126de6e7d91c5f
ColmBhandal <colm.bhandal@canonical.com>
2023-08-29 17:24:03 +0100

    Earlier sed
    Running sed straight after clone
    This ensures it's only run against starter pack
    And it doesn't affect their existing repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,10 +5,17 @@ set +x
 # Ask the user for the installation directory
 read -p "Enter the installation directory (e.g., '.' or 'docs'): " install_directory
 
-# Clone the starter pack repository
+# Clone the starter pack repository to a temporary directory
 echo "Cloning the starter pack repository..."
 git clone git@github.com:canonical/sphinx-docs-starter-pack temp-starter-pack
 rm -rf temp-starter-pack/.git
+
+# Update file contents for install directory
+echo "Updating working directory in workflow files..."
+sed -i "s|working-directory: .*|working-directory: $install_directory|g" "temp-starter-pack/.github/workflows"/*
+echo "Updating .readthedocs.yaml configuration..."
+sed -i "s|configuration: .*conf\.py|configuration: $install_directory/conf.py|g" "temp-starter-pack/.readthedocs.yaml"
+sed -i "s|requirements: .*|requirements: $install_directory/.sphinx/requirements.txt|g" "temp-starter-pack/.readthedocs.yaml"
 
 # Create the specified installation directory
 echo "Creating the installation directory: $install_directory"
@@ -22,15 +29,6 @@ cp -R temp-starter-pack/* temp-starter-pack/.??* "$install_directory"
 echo "Copying workflow files..."
 mkdir -p .github/workflows
 cp -R "$install_directory/.github/workflows"/* .github/workflows
-
-# Update working-directory field in workflow files
-echo "Updating working directory in workflow files..."
-sed -i "s|working-directory: .*|working-directory: $install_directory|g" .github/workflows/*
-
-# Update .readthedocs.yaml configuration
-echo "Updating .readthedocs.yaml configuration..."
-sed -i "s|configuration: .*conf\.py|configuration: $install_directory/conf.py|g" "$install_directory/.readthedocs.yaml"
-sed -i "s|requirements: .*|requirements: $install_directory/.sphinx/requirements.txt|g" "$install_directory/.readthedocs.yaml"
 
 # Clean up
 echo "Cleaning up..."
