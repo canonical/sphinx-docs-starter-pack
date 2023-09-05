7ff5838952a8974d7f292c1fbb89331a2a914950
ColmBhandal <colm.bhandal@canonical.com>
2023-09-05 13:36:45 +0100

    Unique temp dir
    Added timestamp at the end of temp dir
    This guarantees beyond reaonsable doubt it's unique

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,20 +2,24 @@
 
 set +x
 
+# Generate a unique directory name based on timestamp
+timestamp=$(date +%Y%m%d%H%M%S)
+temp_directory="temp-starter-pack-$timestamp"
+
 # Ask the user for the installation directory
 read -p "Enter the installation directory (e.g., '.' or 'docs'): " install_directory
 
-# Clone the starter pack repository to a temporary directory
+# Clone the starter pack repository to the temporary directory
 echo "Cloning the starter pack repository..."
-git clone --depth 1 https://github.com/canonical/sphinx-docs-starter-pack temp-starter-pack
-rm -rf temp-starter-pack/.git
+git clone --depth 1 https://github.com/canonical/sphinx-docs-starter-pack "$temp_directory"
+rm -rf "$temp_directory/.git"
 
-# Update file contents for install directory
+# Update file contents for the install directory
 echo "Updating working directory in workflow files..."
-sed -i "s|working-directory:\s*'\.'|working-directory: '$install_directory'|g" "temp-starter-pack/.github/workflows"/*
+sed -i "s|working-directory:\s*'\.'|working-directory: '$install_directory'|g" "$temp_directory/.github/workflows"/*
 echo "Updating .readthedocs.yaml configuration..."
-sed -i "s|configuration:\s*conf\.py|configuration: $install_directory/conf.py|g" "temp-starter-pack/.readthedocs.yaml"
-sed -i "s|requirements:\s*\.sphinx/requirements\.txt|requirements: $install_directory/.sphinx/requirements.txt|g" "temp-starter-pack/.readthedocs.yaml"
+sed -i "s|configuration:\s*conf\.py|configuration: $install_directory/conf.py|g" "$temp_directory/.readthedocs.yaml"
+sed -i "s|requirements:\s*\.sphinx/requirements\.txt|requirements: $install_directory/.sphinx/requirements.txt|g" "$temp_directory/.readthedocs.yaml"
 
 # Create the specified installation directory
 echo "Creating the installation directory: $install_directory"
@@ -23,7 +27,7 @@ mkdir -p "$install_directory"
 
 # Copy the contents of the starter pack repository to the installation directory
 echo "Copying contents to the installation directory..."
-cp -R temp-starter-pack/* temp-starter-pack/.??* "$install_directory"
+cp -R "$temp_directory"/* "$temp_directory"/.??* "$install_directory"
 
 # Move workflow files and configuration
 if [ "$install_directory" != "." ]; then
@@ -35,6 +39,6 @@ fi
 
 # Clean up
 echo "Cleaning up..."
-rm -rf temp-starter-pack
+rm -rf "$temp_directory"
 
 echo "Setup completed!"
