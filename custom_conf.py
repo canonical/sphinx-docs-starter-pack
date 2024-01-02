58f4d50d023e0215303c80db3b3a4f5ec4484dcd
Ruth Fuchss <ruth.fuchss@canonical.com>
2024-01-02 15:54:56 +0100

    update table alignment in the cheat sheet
    Add examples for centering only some table cells to the cheat
    sheet. This also requires a custom role.
    
    Signed-off-by: Ruth Fuchss <ruth.fuchss@canonical.com>

--- custom_conf.py
+++ custom_conf.py
@@ -169,3 +169,9 @@ custom_tags = []
 ############################################################
 
 ## Add any configuration that is not covered by the common conf.py file.
+
+# Define a :center: role that can be used to center the content of table cells.
+rst_prolog = '''
+.. role:: center
+   :class: align-center
+'''
