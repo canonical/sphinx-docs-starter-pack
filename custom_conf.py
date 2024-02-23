8dbb4eba4ea8cc8a158bed9b6db16d17db25d506
Ruth Fuchss <ruth.fuchss@canonical.com>
2024-02-23 11:14:33 +0100

    Revert "feat: remove notfound.extension"
    This reverts commit 3bd85ffb4c25f9dd662155a679a5baa970182fbd.
    
    Signed-off-by: Ruth Fuchss <ruth.fuchss@canonical.com>

--- custom_conf.py
+++ custom_conf.py
@@ -150,7 +150,8 @@ custom_extensions = [
     'canonical.youtube-links',
     'canonical.related-links',
     'canonical.custom-rst-roles',
-    'canonical.terminal-output'
+    'canonical.terminal-output',
+    'notfound.extension'
     ]
 
 # Add custom required Python modules that must be added to the
@@ -158,7 +159,8 @@ custom_extensions = [
 # NOTE: The following modules are handled automatically and do not need to be
 # added here: canonical-sphinx-extensions, furo, linkify-it-py, myst-parser,
 # pyspelling, sphinx, sphinx-autobuild, sphinx-copybutton, sphinx-design,
-# sphinx-reredirects, sphinx-tabs, sphinxcontrib-jquery, sphinxext-opengraph
+# sphinx-notfound-page, sphinx-reredirects, sphinx-tabs, sphinxcontrib-jquery,
+# sphinxext-opengraph
 custom_required_modules = []
 
 # Add files or directories that should be excluded from processing.
