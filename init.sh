80f153f0bdda68eecd6eef195c96bbff005fec07
Rapo <raviteja.rapolu@gwmail.gwu.edu>
2024-05-30 10:30:56 -0400

    Update init.sh added --ignore-fail-on-non-empty flag
    added --ignore-fail-on-non-empty flag for rmdir to not remove the install_directory if not empty

--- init.sh
+++ init.sh
@@ -37,7 +37,7 @@ if [ "$install_directory" != "." ]; then
         mkdir -p .github/workflows
     fi
     mv "$install_directory/.github/workflows"/* .github/workflows
-    rmdir -p "$install_directory/.github/workflows"
+    rmdir -p --ignore-fail-on-non-empty "$install_directory/.github/workflows"
     if [ ! -f .wokeignore ]; then
         ln -s "$install_directory/.wokeignore"
     else
