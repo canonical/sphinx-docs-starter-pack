78dca478817b310c9980738a4916d3767fb817cd
secondskoll <michael.park@canonical.com>
2025-02-03 11:30:22 +1000

    fix: feedback button

--- docs/conf.py
+++ docs/conf.py
@@ -135,7 +135,7 @@ html_context = {
     # Docs branch in the repo; used in links for viewing the source files
     #
     # TODO: To customise the branch, uncomment and update as needed.
-    # 'github_version': 'main',
+     'github_version': 'main',
     # Docs location in the repo; used in links for viewing the source files
     #
     # TODO: To customise the directory, uncomment and update as needed.
