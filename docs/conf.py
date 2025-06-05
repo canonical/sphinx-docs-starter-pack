5db6fb19f1ca982a361426aa5fe67a851a9a339f
Michael Park <michael.park@canonical.com>
2025-06-06 09:06:12 +1000

    fix: remove outdated comment from conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -180,8 +180,7 @@ html_context = {
 
 html_baseurl = 'https://canonical-starter-pack.readthedocs-hosted.com/'
 
-# URL scheme. Add language and version scheme elements manually e.g. '{0}/{1}/{{link}}'.format(os.environ['READTHEDOCS_LANGUAGE'], os.environ['READTHEDOCS_VERSION'])
-
+# URL scheme. Add language and version scheme elements.
 # When configured with RTD variables, check for RTD environment so manual runs succeed:
 
 if 'READTHEDOCS_VERSION' in os.environ:
