fece8a752019b622f91cbc54891bd1dd2733e5f1
JJ Coldiron <jj.coldiron@canonical.com>
2025-10-31 16:33:21 -0700

    fix(docs): update style guide redirects (#453)

--- docs/conf.py
+++ docs/conf.py
@@ -232,10 +232,10 @@ sitemap_excludes = [
 #       the sphinx_reredirects extension will be disabled.
 
 redirects = {
-    'reference/doc-cheat-sheet-myst/': '/reference/myst-syntax-reference',
-    'reference/doc-cheat-sheet/': '/reference/rst-syntax-reference',
-    'reference/style-guide-myst/': '/reference/myst-syntax-reference',
-    'reference/style-guide/': '/reference/rst-syntax-reference',
+    'reference/doc-cheat-sheet-myst/': '../myst-syntax-reference',
+    'reference/doc-cheat-sheet/': '../rst-syntax-reference',
+    'reference/style-guide-myst/': '../myst-syntax-reference',
+    'reference/style-guide/': '../rst-syntax-reference',
 }
 
 
