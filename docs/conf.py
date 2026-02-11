3da4fba51d8f283440c292b7d6c82127d59ebd86
Yana Hontyk <yana.hontyk@canonical.com>
2026-02-11 15:14:05 +0100

    Add explanation of various components within SP

--- docs/conf.py
+++ docs/conf.py
@@ -299,6 +299,7 @@ extensions = [
     "sphinx_last_updated_by_git",
     "sphinx.ext.intersphinx",
     "sphinx_sitemap",
+    "sphinxcontrib.mermaid",
 ]
 
 # Excludes files or directories from processing
