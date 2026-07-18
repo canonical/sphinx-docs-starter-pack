edea2411effd9bf5a6c17870fa719ae1b6eaf794
Michael DuBelko <michael.dubelko@canonical.com>
2026-07-17 17:11:16 -0700

    fix: make contributor listing work

--- docs/conf.py
+++ docs/conf.py
@@ -78,6 +78,7 @@ html_context = {
     "repo_default_branch": "main",
     # Docs location in the repo; used in links for viewing the source files
     "github_folder": "/docs/",
+    "repo_folder": "/docs/",
     # TODO: To enable or disable the Previous / Next buttons at the bottom of pages
     # Valid options: none, prev, next, both
     # "sequential_nav": "",
